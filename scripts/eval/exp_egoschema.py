586294ca909c8568e518e9e8f6cd456868c9cda2
steven-ccq <55176896+steven-ccq@users.noreply.github.com>
2025-04-01 16:13:35 +0800

    Update exp_egoschema.py

--- scripts/eval/exp_egoschema.py
+++ scripts/eval/exp_egoschema.py
@@ -107,7 +107,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('--dataset_path', default='dataset/egoschema/egoschema_val.json')
     arg_parser.add_argument('--video_dir', default='dataset/egoschema/videos')
     arg_parser.add_argument('--output_dir', default='dataset/egoschema/output')
-    arg_parser.add_argument('--version', default='tmp')
+    arg_parser.add_argument('--version', default='ViLAMP-llava-qwen')
     arg_parser.add_argument('--split', default='1_1')
     arg_parser.add_argument('--max_frame_num', type=int, default=600)
 
